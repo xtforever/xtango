d30364177631de9dd2b007ee1742183526292617
Harms <wharms@bfs.de>
2021-02-02 22:46:59 +0100

    change indent

--- src/xtangotap.c
+++ src/xtangotap.c
@@ -25,35 +25,26 @@
 /*******************    LOCAL functions    *******************/
 /*************************************************************/
 
-
-
-
-
 /*************************************************************/
 /* TAPfill -- Fills a specified image to a solid color       */
 /*                                                           */
 /* RETURNS:  The transition required to perform the fill     */
 /*************************************************************/
-TANGO_TRANS
-TAPfill(image)
-   TANGO_IMAGE image;
+TANGO_TRANS TAPfill(image)
+TANGO_IMAGE image;
 {
-TANGO_PATH fill_path;
-TANGO_TRANS fill_trans;
-double x[1], y[1];
+	TANGO_PATH fill_path;
+	TANGO_TRANS fill_trans;
+	double x[1], y[1];
 
-x[0] = 1.0;
-y[0] = 0.0;
+	x[0] = 1.0;
+	y[0] = 0.0;
 
-fill_path = TANGOpath_create(1, x, y);
-fill_trans = TANGOtrans_create(TANGO_TRANS_TYPE_FILL, image, fill_path);
-TANGOpath_free(1, fill_path);
-return(fill_trans);
+	fill_path = TANGOpath_create(1, x, y);
+	fill_trans = TANGOtrans_create(TANGO_TRANS_TYPE_FILL, image, fill_path);
+	TANGOpath_free(1, fill_path);
+	return (fill_trans);
 }
-
-
-
-
 
 /************************************************************/
 /* TAPcolor -- Changes the specified image to the given     */
@@ -62,24 +53,20 @@ return(fill_trans);
 /* RETURNS:  The transition required to perform the color   */
 /*           change                                         */
 /************************************************************/
-TANGO_TRANS
-TAPcolor(image, color)
-   TANGO_IMAGE image;
-   TANGO_COLOR color;
+TANGO_TRANS TAPcolor(image, color)
+TANGO_IMAGE image;
+TANGO_COLOR color;
 
 {
-TANGO_PATH color_path;
-TANGO_TRANS color_trans;
+	TANGO_PATH color_path;
+	TANGO_TRANS color_trans;
 
-color_path = TANGOpath_color(color);
-color_trans = TANGOtrans_create(TANGO_TRANS_TYPE_COLOR, image, color_path);
-TANGOpath_free(1, color_path);
-return(color_trans);
+	color_path = TANGOpath_color(color);
+	color_trans =
+	    TANGOtrans_create(TANGO_TRANS_TYPE_COLOR, image, color_path);
+	TANGOpath_free(1, color_path);
+	return (color_trans);
 }
-
-
-
-
 
 /*************************************************************/
 /* TAPshow -- Displays an image; that is, performs a delay   */
@@ -87,50 +74,44 @@ return(color_trans);
 /*                                                           */
 /* RETURNS:  None.                                           */
 /*************************************************************/
-void
-TAPshow(image)
-   TANGO_IMAGE image;
+void TAPshow(image)
+TANGO_IMAGE image;
 
 /* Displays an image */
 
 {
-TANGO_PATH null_path;
-TANGO_TRANS disp_trans;
+	TANGO_PATH null_path;
+	TANGO_TRANS disp_trans;
 
-null_path = TANGOpath_null(1);
-disp_trans = TANGOtrans_create(TANGO_TRANS_TYPE_DELAY, image, null_path);
+	null_path = TANGOpath_null(1);
+	disp_trans =
+	    TANGOtrans_create(TANGO_TRANS_TYPE_DELAY, image, null_path);
 
-TANGOtrans_perform(disp_trans);
-TANGOpath_free(1, null_path);
-TANGOtrans_free(1, disp_trans);
+	TANGOtrans_perform(disp_trans);
+	TANGOpath_free(1, null_path);
+	TANGOtrans_free(1, disp_trans);
 }
-
-
-
-
 
 /*************************************************************/
 /* TAPvis_toggle -- Toggles the visibility of an object      */
 /*                                                           */
 /* RETURNS:  The required transition                         */
 /*************************************************************/
-TANGO_TRANS
-TAPvis_toggle(image)
-   TANGO_IMAGE image;
+TANGO_TRANS TAPvis_toggle(image)
+TANGO_IMAGE image;
 
 /* Toggles the visibility of an image */
 
 {
-TANGO_PATH null_path;
-TANGO_TRANS visi_trans;
+	TANGO_PATH null_path;
+	TANGO_TRANS visi_trans;
 
-null_path = TANGOpath_null(1);
-visi_trans = TANGOtrans_create(TANGO_TRANS_TYPE_VISIBLE, image, null_path);
-TANGOpath_free(1, null_path);
-return(visi_trans);
+	null_path = TANGOpath_null(1);
+	visi_trans =
+	    TANGOtrans_create(TANGO_TRANS_TYPE_VISIBLE, image, null_path);
+	TANGOpath_free(1, null_path);
+	return (visi_trans);
 }
-
-
 
 /*************************************************************/
 /* TAPjump -- Sets up the transition to change and objects   */
@@ -138,34 +119,29 @@ return(visi_trans);
 /*                                                           */
 /* RETURNS:  The required transition                         */
 /*************************************************************/
-TANGO_TRANS
-TAPjump(image, part, location)
-   TANGO_IMAGE image;
-   TANGO_PART_TYPE part;
-   TANGO_LOC location;
+TANGO_TRANS TAPjump(image, part, location)
+TANGO_IMAGE image;
+TANGO_PART_TYPE part;
+TANGO_LOC location;
 
 {
-TANGO_PATH jump_path;
-TANGO_TRANS jump_trans;
-TANGO_LOC image_loc;
-double xoffset[1], yoffset[1];
+	TANGO_PATH jump_path;
+	TANGO_TRANS jump_trans;
+	TANGO_LOC image_loc;
+	double xoffset[1], yoffset[1];
 
-image_loc = TANGOimage_loc(image, part);
+	image_loc = TANGOimage_loc(image, part);
 
-xoffset[0] = TANGOloc_X(location) - TANGOloc_X(image_loc);
-yoffset[0] = TANGOloc_Y(location) - TANGOloc_Y(image_loc);
+	xoffset[0] = TANGOloc_X(location) - TANGOloc_X(image_loc);
+	yoffset[0] = TANGOloc_Y(location) - TANGOloc_Y(image_loc);
 
-jump_path = TANGOpath_create(1, xoffset, yoffset);
+	jump_path = TANGOpath_create(1, xoffset, yoffset);
 
-jump_trans = TANGOtrans_create(TANGO_TRANS_TYPE_MOVE, image, jump_path);
-TANGOpath_free(1, jump_path);
-free(image_loc);
-return(jump_trans);
+	jump_trans = TANGOtrans_create(TANGO_TRANS_TYPE_MOVE, image, jump_path);
+	TANGOpath_free(1, jump_path);
+	free(image_loc);
+	return (jump_trans);
 }
-
-
-
-
 
 /*************************************************************/
 /* TAPmove -- sets up the transition to move an object to the*/
@@ -174,30 +150,26 @@ return(jump_trans);
 /*                                                           */
 /* RETURNS:  The move transition.                            */
 /*************************************************************/
-TANGO_TRANS
-TAPmove(image, part, location)
-   TANGO_IMAGE image;
-   TANGO_PART_TYPE part;
-   TANGO_LOC location;
+TANGO_TRANS TAPmove(image, part, location)
+TANGO_IMAGE image;
+TANGO_PART_TYPE part;
+TANGO_LOC location;
 
 {
-TANGO_PATH move_path;
-TANGO_TRANS move_trans;
-TANGO_LOC image_loc;
+	TANGO_PATH move_path;
+	TANGO_TRANS move_trans;
+	TANGO_LOC image_loc;
 
-image_loc = TANGOimage_loc(image, part);
+	image_loc = TANGOimage_loc(image, part);
 
-move_path = TANGOpath_motion(image_loc, location, TANGO_PATH_TYPE_STRAIGHT);
-move_trans = TANGOtrans_create(TANGO_TRANS_TYPE_MOVE, image, move_path);
-TANGOpath_free(1, move_path);
+	move_path =
+	    TANGOpath_motion(image_loc, location, TANGO_PATH_TYPE_STRAIGHT);
+	move_trans = TANGOtrans_create(TANGO_TRANS_TYPE_MOVE, image, move_path);
+	TANGOpath_free(1, move_path);
 
-free(image_loc);
-return(move_trans);
+	free(image_loc);
+	return (move_trans);
 }
-
-
-
-
 
 /*************************************************************/
 /* TAPtraverse -- cause an image to traverse a given         */
@@ -206,31 +178,27 @@ return(move_trans);
 /*                                                           */
 /* RETURNS:  The required transition                         */
 /*************************************************************/
-TANGO_TRANS
-TAPtraverse(image, part, location, motion)
-   TANGO_IMAGE image;
-   TANGO_PART_TYPE part;
-   TANGO_LOC location;
-   TANGO_PATH_TYPE motion;
+TANGO_TRANS TAPtraverse(image, part, location, motion)
+TANGO_IMAGE image;
+TANGO_PART_TYPE part;
+TANGO_LOC location;
+TANGO_PATH_TYPE motion;
 
 {
-TANGO_PATH traverse_path;
-TANGO_TRANS traverse_trans;
-TANGO_LOC image_loc;
+	TANGO_PATH traverse_path;
+	TANGO_TRANS traverse_trans;
+	TANGO_LOC image_loc;
 
-image_loc = TANGOimage_loc(image, part);
+	image_loc = TANGOimage_loc(image, part);
 
-traverse_path = TANGOpath_motion(image_loc, location, motion);
-traverse_trans = TANGOtrans_create(TANGO_TRANS_TYPE_MOVE, image, traverse_path);
-TANGOpath_free(1, traverse_path);
+	traverse_path = TANGOpath_motion(image_loc, location, motion);
+	traverse_trans =
+	    TANGOtrans_create(TANGO_TRANS_TYPE_MOVE, image, traverse_path);
+	TANGOpath_free(1, traverse_path);
 
-free(image_loc);
-return(traverse_trans);
+	free(image_loc);
+	return (traverse_trans);
 }
-
-
-
-
 
 /*************************************************************/
 /* TAPswitch -- sets up the transition necessary to swap the */
@@ -238,52 +206,47 @@ return(traverse_trans);
 /*                                                           */
 /* RETURNS:  The switch transition                           */
 /*************************************************************/
-TANGO_TRANS
-TAPswitch(image1, image2)
-   TANGO_IMAGE image1;
-   TANGO_IMAGE image2;
+TANGO_TRANS TAPswitch(image1, image2)
+TANGO_IMAGE image1;
+TANGO_IMAGE image2;
 
 {
-TANGO_LOC image1_loc;
-TANGO_LOC image2_loc;
-TANGO_PATH switch_path1;
-TANGO_PATH switch_path2;
-TANGO_TRANS switch_trans1;
-TANGO_TRANS switch_trans2;
-TANGO_TRANS total_trans;
-double xoffset[2], yoffset[2];
+	TANGO_LOC image1_loc;
+	TANGO_LOC image2_loc;
+	TANGO_PATH switch_path1;
+	TANGO_PATH switch_path2;
+	TANGO_TRANS switch_trans1;
+	TANGO_TRANS switch_trans2;
+	TANGO_TRANS total_trans;
+	double xoffset[2], yoffset[2];
 
-image1_loc = TANGOimage_loc(image1, TANGO_PART_TYPE_C);
-image2_loc = TANGOimage_loc(image2, TANGO_PART_TYPE_C);
+	image1_loc = TANGOimage_loc(image1, TANGO_PART_TYPE_C);
+	image2_loc = TANGOimage_loc(image2, TANGO_PART_TYPE_C);
 
-xoffset[0] = yoffset[0] = (double)0.0;
+	xoffset[0] = yoffset[0] = (double)0.0;
 
-xoffset[1] = TANGOloc_X(image2_loc) - TANGOloc_X(image1_loc);
-yoffset[1] = TANGOloc_Y(image2_loc) - TANGOloc_Y(image1_loc);
+	xoffset[1] = TANGOloc_X(image2_loc) - TANGOloc_X(image1_loc);
+	yoffset[1] = TANGOloc_Y(image2_loc) - TANGOloc_Y(image1_loc);
 
-switch_path1 = TANGOpath_create(2, xoffset, yoffset);
-switch_trans1 = TANGOtrans_create(TANGO_TRANS_TYPE_MOVE, image1,
-                        switch_path1);
+	switch_path1 = TANGOpath_create(2, xoffset, yoffset);
+	switch_trans1 = TANGOtrans_create(TANGO_TRANS_TYPE_MOVE, image1,
+					  switch_path1);
 
-xoffset[1] = TANGOloc_X(image1_loc) - TANGOloc_X(image2_loc);
-yoffset[1] = TANGOloc_Y(image1_loc) - TANGOloc_Y(image2_loc);
+	xoffset[1] = TANGOloc_X(image1_loc) - TANGOloc_X(image2_loc);
+	yoffset[1] = TANGOloc_Y(image1_loc) - TANGOloc_Y(image2_loc);
 
-switch_path2 = TANGOpath_create(2, xoffset, yoffset);
-switch_trans2 = TANGOtrans_create(TANGO_TRANS_TYPE_MOVE, image2,
-                        switch_path2);
-TANGOpath_free(2, switch_path1, switch_path2);
+	switch_path2 = TANGOpath_create(2, xoffset, yoffset);
+	switch_trans2 = TANGOtrans_create(TANGO_TRANS_TYPE_MOVE, image2,
+					  switch_path2);
+	TANGOpath_free(2, switch_path1, switch_path2);
 
-total_trans = TANGOtrans_compose(2, switch_trans1, switch_trans2);
+	total_trans = TANGOtrans_compose(2, switch_trans1, switch_trans2);
 
-free(image1_loc);
-free(image2_loc);
-TANGOtrans_free(2, switch_trans1, switch_trans2);
-return(total_trans);
+	free(image1_loc);
+	free(image2_loc);
+	TANGOtrans_free(2, switch_trans1, switch_trans2);
+	return (total_trans);
 }
-
-
-
-
 
 /*************************************************************/
 /* TAPexchange -- sets up the transition necessary to swap   */
@@ -292,46 +255,41 @@ return(total_trans);
 /*                                                           */
 /* RETURNS: The transition                                   */
 /*************************************************************/
-TANGO_TRANS
-TAPexchange(image1, image2)
-   TANGO_IMAGE image1;
-   TANGO_IMAGE image2;
+TANGO_TRANS TAPexchange(image1, image2)
+TANGO_IMAGE image1;
+TANGO_IMAGE image2;
 
 {
-TANGO_LOC image1_loc;
-TANGO_LOC image2_loc;
-TANGO_PATH exchange_path1;
-TANGO_PATH exchange_path2;
-TANGO_TRANS exchange_trans1;
-TANGO_TRANS exchange_trans2;
-TANGO_TRANS total_trans;
+	TANGO_LOC image1_loc;
+	TANGO_LOC image2_loc;
+	TANGO_PATH exchange_path1;
+	TANGO_PATH exchange_path2;
+	TANGO_TRANS exchange_trans1;
+	TANGO_TRANS exchange_trans2;
+	TANGO_TRANS total_trans;
 
-image1_loc = TANGOimage_loc(image1, TANGO_PART_TYPE_C);
-image2_loc = TANGOimage_loc(image2, TANGO_PART_TYPE_C);
+	image1_loc = TANGOimage_loc(image1, TANGO_PART_TYPE_C);
+	image2_loc = TANGOimage_loc(image2, TANGO_PART_TYPE_C);
 
-exchange_path1 = TANGOpath_motion(image1_loc, image2_loc, 
-                                 TANGO_PATH_TYPE_STRAIGHT);
-                                 
-exchange_trans1 = TANGOtrans_create(TANGO_TRANS_TYPE_MOVE, image1,
-                        exchange_path1);
+	exchange_path1 = TANGOpath_motion(image1_loc, image2_loc,
+					  TANGO_PATH_TYPE_STRAIGHT);
 
-exchange_path2 = TANGOpath_motion(image2_loc, image1_loc,
-                                 TANGO_PATH_TYPE_STRAIGHT);
-exchange_trans2 = TANGOtrans_create(TANGO_TRANS_TYPE_MOVE, image2,
-                        exchange_path2);
-TANGOpath_free(2, exchange_path1, exchange_path2);
+	exchange_trans1 = TANGOtrans_create(TANGO_TRANS_TYPE_MOVE, image1,
+					    exchange_path1);
 
-total_trans = TANGOtrans_compose(2, exchange_trans1, exchange_trans2);
+	exchange_path2 = TANGOpath_motion(image2_loc, image1_loc,
+					  TANGO_PATH_TYPE_STRAIGHT);
+	exchange_trans2 = TANGOtrans_create(TANGO_TRANS_TYPE_MOVE, image2,
+					    exchange_path2);
+	TANGOpath_free(2, exchange_path1, exchange_path2);
 
-free(image1_loc);
-free(image2_loc);
-TANGOtrans_free(2, exchange_trans1, exchange_trans2);
-return(total_trans);
+	total_trans = TANGOtrans_compose(2, exchange_trans1, exchange_trans2);
+
+	free(image1_loc);
+	free(image2_loc);
+	TANGOtrans_free(2, exchange_trans1, exchange_trans2);
+	return (total_trans);
 }
-
-
-
-
 
 /*************************************************************/
 /* TAPflash -- causes an image to flash on and off the       */
@@ -339,17 +297,17 @@ return(total_trans);
 /*                                                           */
 /* RETURNS: the flash transition                             */
 /*************************************************************/
-TANGO_TRANS
-TAPflash(image, num_flash)
-   TANGO_IMAGE image;
-   int num_flash;
+TANGO_TRANS TAPflash(image, num_flash)
+TANGO_IMAGE image;
+int num_flash;
 
 {
-TANGO_PATH null_path;
-TANGO_TRANS flash_trans;
+	TANGO_PATH null_path;
+	TANGO_TRANS flash_trans;
 
-null_path = TANGOpath_null(num_flash * 2);
-flash_trans = TANGOtrans_create(TANGO_TRANS_TYPE_VISIBLE, image, null_path);
-TANGOpath_free(1, null_path);
-return(flash_trans);
+	null_path = TANGOpath_null(num_flash * 2);
+	flash_trans =
+	    TANGOtrans_create(TANGO_TRANS_TYPE_VISIBLE, image, null_path);
+	TANGOpath_free(1, null_path);
+	return (flash_trans);
 }
